c87b76631c6b5e831b928761af0309bd3e1ed8fb
Arjun Rao <91220815+ItsWitchDoctor@users.noreply.github.com>
2026-03-24 23:35:55 +0000

    finished part 1 - fixed producer and consumer

---
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,40 @@
+import pika 
+import os 
+from consumer_interface import mqConsumerInterface
+
+
+class mqConsumer(mqConsumerInterface):
+
+    def __init__(self, binding_key, exchange_name, queue_name):
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        self.setupRMQConnection()
+    
+    def setupRMQConnection(self):
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        self.connection = pika.BlockingConnection(parameters=conParams)
+
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange_name)
+
+        self.channel.queue_declare(queue=self.queue_name)
+        self.channel.queue_bind(
+            queue= self.queue_name,
+            routing_key= self.binding_key,
+            exchange= self.exchange_name,
+        )
+
+        self.channel.basic_consume(self.queue_name, on_message_callback=self.on_message_callback, auto_ack=False)
+
+    def on_message_callback(self, channel, method_frame, header_frame, body):
+        channel.basic_ack(method_frame.delivery_tag, False)
+        print(body)
+        channel.close()
+    
+    def startConsuming(self):
+        print("[*] Waiting for messages. To exit press CTRL+C")
+        self.channel.start_consuming()
+    
+    def __del__(self):
+        self.connection.close()
